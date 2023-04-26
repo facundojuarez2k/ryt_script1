dbd8f9430dc87987f96f5bb12375748cb8330716
Facundo Juarez <facundojuarezf@outlook.com>
2023-04-26 14:40:51 -0300

    Agrega logica script

--- arpping.py
+++ arpping.py
@@ -1,9 +1,40 @@
 import sys
+import argparse
 #from scapy import Scapy
 
+
 def main():
-    print("Hello")
+    try:
+        args = parse_args()
+        validate_args(args)
+        print(args.ip_address)
+        print(args.count)
+    except ValueError as ex1:
+        print(f'ERROR: {str(ex1)}', file=sys.stderr)
     return 0
+
+
+def parse_args():
+    '''
+    Captura y retorna los argumentos del programa
+    '''
+    parser = argparse.ArgumentParser(description='ARP Ping')
+    parser.add_argument(dest='ip_address', type=str, help='Dirección IPv4 a consultar')
+    parser.add_argument('--count', '-c', dest='count', type=int,
+                        help='Cantidad de mensajes ARP who-has. Admite valores enteros mayores o iguales a 0. El valor 0 equivale a una cantidad infinita. (Default = 0) (Opcional)', default=0)
+    parser.add_argument('--device', '-d', dest='device', type=int,
+                        help='Interfaz de red a utilizar (Requerido)', required=True)
+    return parser.parse_args()
+
+
+def validate_args(args: object):
+    '''
+    Valida los argumentos del programa
+    '''
+    if not hasattr(args, 'count') or args.count < 0:
+        raise ValueError(
+            'El argumento count debe ser un entero mayor o igual a cero')
+
 
 if __name__ == '__main__':
     main()
