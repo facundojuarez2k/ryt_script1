a7a04eabf90c25f17f34336b0704991fcba84ac7
Facundo Juarez <facundojuarezf@outlook.com>
2023-04-26 14:05:05 -0300

    Boilerplate

---
+++ arpping.py
@@ -0,0 +1,9 @@
+import sys
+#from scapy import Scapy
+
+def main():
+    print("Hello")
+    return 0
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test_arpping.py
@@ -0,0 +1,3 @@
+class TestARPPing:
+    def test_fail(self):
+      assert 0==1
